1a53854d6b29b80aba3957f338a6b4643b3f0cfa
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2025-06-24 10:25:04 -0300

    navbar and constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export function App() {
   return (
-    <div className=" flex-center h-[100vh]">
-      <h1 className=" text-3xl text-indigo-300">Welcome to the React App</h1>
-      <p>This is a simple React application.</p>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 }
